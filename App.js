344ab46fb53096c49b907428f53a2975bc5e9942
Clifford <kingsleyclifford2016@gmail.com>
2021-08-21 04:40:44 +0000

    added line of code

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Shopping from "./src/screen/Shopping";
+import Cart from "./src/screen/Cart";
+import Payment from "./src/screen/Payment";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Shopping></Shopping> */}
+      {/* <Cart></Cart> */}
+      <Payment></Payment>
     </View>
   );
 }
@@ -14,8 +18,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ src/screen/Shopping.js
@@ -0,0 +1,142 @@
+import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+
+const Shopping = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.mainContainer}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>ONLINE SHOPPING</Text>
+          <Text style={styles.subText}>
+            Ea aliquip laborum exercitation nostrud magna proident non sint
+            dolore exercitation ad in. Nostrud in non officia enim. Laborum
+            mollit pariatur ut occaecat ut nostrud excepteur. Culpa pariatur
+            cupidatat elit velit proident laborum aliquip ex exercitation
+            excepteur.
+          </Text>
+        </View>
+
+        <View style={styles.imageBox}>
+          <Image
+            style={styles.image}
+            source={require("../../assets/shopping.png")}
+          ></Image>
+        </View>
+
+        <View style={styles.btnBox}>
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btnText}>Next</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.skipView}>
+          <View></View>
+          <View style={styles.pages}>
+            <View style={styles.currentPage}></View>
+            <View style={styles.page}></View>
+            <View style={styles.page}></View>
+          </View>
+          <TouchableOpacity style={styles.skip}>
+            <Text style={styles.skipText}>Skip</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  header: {
+    marginTop: 80,
+  },
+
+  imageBox: {
+    alignItems: "center",
+  },
+
+  image: {
+    width: 320,
+    height: 320,
+  },
+
+  pages: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: 47,
+    justifyContent: "space-between",
+  },
+
+  skipText: {
+    color: "grey",
+    fontWeight: "bold",
+  },
+
+  skipView: {
+    marginTop: 75,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  page: {
+    width: 7,
+    height: 7,
+    backgroundColor: "#c9b1f0",
+    borderRadius: 100,
+  },
+
+  currentPage: {
+    width: 20,
+    height: 7,
+    backgroundColor: "#480ca8",
+    borderRadius: 50,
+  },
+
+  headerText: {
+    fontSize: 25,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+
+  btnBox: {
+    alignItems: "center",
+  },
+
+  subText: {
+    fontSize: 16,
+  },
+
+  btn: {
+    backgroundColor: "#480ca8",
+    width: 160,
+    height: 50,
+    borderRadius: 50,
+    marginTop: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  btnText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+
+  mainContainer: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+});
+
+export default Shopping;
